ee41718d1c27b93a127cd7d467c51dfa64c8379c
nirmalraaj0275 <nirmalraaj0275@gmail.com>
2024-10-20 14:02:35 +0530

    new

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ function addtask() {
     alert("Please enter a task!!");
     return;
   }
+
   let taskList = document.getElementById("taskList");
   let li = document.createElement("li");
   li.textContent = taskInput;
 
   let deletebutton = document.createElement("button");
-  deletebutton.textContent = "delete";
-  deletebutton.classList.add("delect-btns");
+  deletebutton.textContent = "Delete";
+  deletebutton.classList.add("delete-btn");
   deletebutton.onclick = function () {
     taskList.removeChild(li);
   };
-  li.onlick = function () {
-    li.classList.toggle("Completed");
+
+  li.onclick = function () {
+    li.classList.toggle("completed");
   };
+
   li.appendChild(deletebutton);
   taskList.appendChild(li);
+
   document.getElementById("taskInput").value = "";
 }
